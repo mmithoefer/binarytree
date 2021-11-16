6221832bc0711c436c62d9026cc54835805cdd09
mmithoefer <moritz-m@t-online.de>
2021-11-16 08:24:47 +0100

    binaryTree funktioniert

--- src/binaryTree.java
+++ src/binaryTree.java
@@ -1,9 +1,39 @@
 public class binaryTree {
-    private class node{
-        private int val;
-        private node left, right;
-        public node(int val){
+
+    private class Node {
+        public int val;
+        private Node left, right;
+
+        public Node(int val) {
             this.val = val;
         }
     }
+
+    public Node root;
+
+    public binaryTree() {
+        root = null;
+    }
+
+    public binaryTree(int val) {
+        root = new Node(val);
+    }
+
+    public binaryTree(Node node) {
+        this.root = node;
+    }
+
+    public void setLeft(binaryTree treeLeft) {
+        if (treeLeft.isEmpty() || isEmpty()) throw new RuntimeException();
+        this.root.left = treeLeft.root;
+    }
+    public void setRight(binaryTree treeRight) {
+        if (treeRight.isEmpty() || isEmpty()) throw new RuntimeException();
+        this.root.left = treeRight.root;
+    }
+
+    public boolean isEmpty(){
+        return this.root == null;
+    }
 }
+
