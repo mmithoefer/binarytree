05616efa30b1fd84fc114da59fdd6096d5d00479
mmithoefer <moritz-m@t-online.de>
2021-11-16 09:19:20 +0100

    traversierung added

--- src/binaryTree.java
+++ src/binaryTree.java
@@ -29,11 +29,76 @@ public class binaryTree {
     }
     public void setRight(binaryTree treeRight) {
         if (treeRight.isEmpty() || isEmpty()) throw new RuntimeException();
-        this.root.left = treeRight.root;
+        this.root.right = treeRight.root;
     }
 
     public boolean isEmpty(){
         return this.root == null;
+    }
+
+    public binaryTree getLeft(){
+        return new binaryTree(this.root.left);
+    }
+
+    public binaryTree getRight(){
+        return new binaryTree(this.root.right);
+    }
+
+    public void setVal(int val){
+        this.root.val = val;
+    }
+
+    public int getVal(){
+        return this.root.val;
+    }
+
+    public boolean isLeaf(){
+        return this.root.left == null && this.root.right == null;
+    }
+
+    public String Inorder(binaryTree tree){
+        String ausgabe = "";
+        if (!tree.isEmpty()){
+            ausgabe = ausgabe.concat(Inorder(tree.getLeft()));
+            ausgabe = ausgabe.concat(Integer.toString(tree.getVal()));
+            ausgabe = ausgabe.concat(Inorder(tree.getRight()));
+        }
+        return ausgabe;
+    }
+
+    public String Preorder(binaryTree tree){
+        String ausgabe = "";
+        if (!tree.isEmpty()){
+            ausgabe = ausgabe.concat(Integer.toString(tree.getVal()));
+            ausgabe = ausgabe.concat(Inorder(tree.getLeft()));
+            ausgabe = ausgabe.concat(Inorder(tree.getRight()));
+        }
+        return ausgabe;
+    }
+
+    public String Postorder(binaryTree tree){
+        String ausgabe = "";
+        if (!tree.isEmpty()){
+            ausgabe = ausgabe.concat(Inorder(tree.getLeft()));
+            ausgabe = ausgabe.concat(Inorder(tree.getRight()));
+            ausgabe = ausgabe.concat(Integer.toString(tree.getVal()));
+        }
+        return ausgabe;
+    }
+
+    public static void main(String[] args) {
+        binaryTree a = new binaryTree(1);
+        binaryTree b = new binaryTree(2);
+        binaryTree c = new binaryTree(3);
+        binaryTree d = new binaryTree(4);
+        binaryTree e = new binaryTree(5);
+        a.setLeft(b);
+        a.setRight(c);
+        b.setRight(d);
+        b.setLeft(e);
+        System.out.println("Inorder: " + a.Inorder(a));
+        System.out.println("Preorder: " + a.Preorder(a));
+        System.out.println("Postorder: "+ a.Postorder(a));
     }
 }
 
