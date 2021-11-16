dfb8a4e24f366654c0c1d0329f2ce96e4302a768
mmithoefer <moritz-m@t-online.de>
2021-11-16 08:00:39 +0100

    Initial commit

---
+++ src/binaryTree.java
@@ -0,0 +1,9 @@
+public class binaryTree {
+    private class node{
+        private int val;
+        private node left, right;
+        public node(int val){
+            this.val = val;
+        }
+    }
+}
